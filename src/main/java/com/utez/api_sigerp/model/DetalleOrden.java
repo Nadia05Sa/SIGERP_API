f7d79e1e1ee5e3d5dee3bcdb8cc95cbed2b6fef0
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2025-04-10 05:55:55 -0600

    controller cambiado

--- src/main/java/com/utez/api_sigerp/model/DetalleOrden.java
+++ src/main/java/com/utez/api_sigerp/model/DetalleOrden.java
@@ -11,6 +11,16 @@ public class DetalleOrden {
 
     private int cantidad;
 
+    private String detalle;
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
     public Producto getProducto() {
         return producto;
     }
